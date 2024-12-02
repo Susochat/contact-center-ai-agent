58ace1b94a84215380df568227dbf9cfe65f66cc
Susochat <103995980+Susochat@users.noreply.github.com>
2024-12-02 12:12:58 +0530

    updated frontend/src

--- contact-center-ai-agent/frontend/src/App.js
+++ contact-center-ai-agent/frontend/src/App.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Chat from './pages/Chat';
+import Home_dgpt2 from './pages/Home_dgpt2';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/" element={<Chat />} /> */}
+        <Route path="/" element={<Home />} /> 
+        {/* <Route path="/" element={<Home_dgpt2 />} /> */}
       </Routes>
     </Router>
   );
